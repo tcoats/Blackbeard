f8e7bcbd70c061d48e1fe12e82b1f1230cb68e8a
Luke McGregor <luke@mcgregor.org.nz>
2012-09-11 22:37:03 +1200

    upgraded nuget stuff

--- Site/Scripts/_references.js
+++ Site/Scripts/_references.js
@@ -1,4 +1,5 @@
-﻿/// <reference path="jquery-1.6.2.js" />
+﻿/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="jquery-1.8.1.js" />
 /// <reference path="jquery-ui-1.8.11.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
