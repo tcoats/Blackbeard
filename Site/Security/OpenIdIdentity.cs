8a9194f8ec0edadf3c685a65a3ef58e4fa60d0af
Luke <McGregor>
2012-09-11 19:10:38 +1200

    Added gravatar

--- Site/Security/OpenIdIdentity.cs
+++ Site/Security/OpenIdIdentity.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Principal;
+using System.Text;
 using System.Web;
 using SocialPirates.Blackbeard.Site.Models;
 
@@ -10,10 +12,16 @@ namespace SocialPirates.Blackbeard.Site.Security
     public class OpenIdIdentity : IIdentity
     {
         private readonly OpenIdUser _user;
+		private readonly string _gravatarUrl;
 
         public OpenIdIdentity(OpenIdUser user)
         {
             _user = user;
+
+			var md5 = MD5.Create();
+			var hashedEmail = BitConverter.ToString(md5.ComputeHash(new UTF8Encoding().GetBytes(user.Email.Trim().ToLower()))).Replace("-","").ToLower();
+
+			_gravatarUrl = String.Format("http://www.gravatar.com/avatar/{0}", hashedEmail);
         }
 
         public OpenIdUser OpenIdUser
@@ -47,5 +55,13 @@ namespace SocialPirates.Blackbeard.Site.Security
                 return _user.Nickname ?? string.Empty;
             }
         }
+
+		public string GravatarUrl
+		{
+			get
+			{
+				return _gravatarUrl;
+			}
+		}
     }
 }
