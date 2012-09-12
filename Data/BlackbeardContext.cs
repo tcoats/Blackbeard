b0b7a246b5e536c3e2fbddf1d6e11366cffb4e63
Luke McGregor <luke@mcgregor.org.nz>
2012-09-12 20:47:26 +1200

    Added EF Migrations

--- Data/BlackbeardContext.cs
+++ Data/BlackbeardContext.cs
@@ -13,5 +13,20 @@ namespace SocialPirates.Blackbeard.Data
 
 		public DbSet<User> Users { get; set; }
 		public DbSet<Project> Projects { get; set; }
+		public DbSet<Measure> Measures { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Project>().HasMany(p => p.Conceivers).WithMany().Map(m=>{
+				m.ToTable("ProjectConeivers");
+				m.MapLeftKey("ProjectId");
+				m.MapRightKey("UserId");
+			});
+			modelBuilder.Entity<Project>().HasMany(p => p.Contributors).WithMany().Map(m=>{
+				m.ToTable("ProjectContributers");
+				m.MapLeftKey("ProjectId");
+				m.MapRightKey("UserId");
+			});
+		}
 	}
 }
---
+++ Site/App_Start/EntityFrameworkConfiguration.cs
@@ -0,0 +1,24 @@
+﻿[assembly: WebActivator.PreApplicationStartMethod(typeof(SocialPirates.Blackbeard.Site.App_Start.EntityFrameworkConfiguration), "Start")]
+
+namespace SocialPirates.Blackbeard.Site.App_Start
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Data.Entity;
+	using System.Linq;
+	using System.Web;
+	using SocialPirates.Blackbeard.Data;
+	using SocialPirates.Blackbeard.Data.Migrations;
+
+	public class EntityFrameworkConfiguration
+	{
+		/// <summary>
+		/// Starts the application
+		/// </summary>
+		public static void Start()
+		{
+			Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlackbeardContext, Configuration>());
+		}
+
+	}
+}
